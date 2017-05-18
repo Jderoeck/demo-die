3c50862a4b2949778be180f2be8e52d730297651
MartenFelis <marten.felis@hotmail.com>
2017-05-18 23:57:24 +0200

    fix

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,52 +3,36 @@ const users = require('../controllers/user');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res , next) {
-  //console.log('test');
-  //console.log('users', users.list);
-  res.render('index', {users: users.list});
-  users.list(req, res, next);
-});
-
-router.get('/layout', function(req, res, next) {
-  res.render('layout', {users: users.list});
-  users.list(req, res, next);
+router.get('/', function(req, res, next) {
+    //console.log('test');
+    //console.log('users', users.list);
+    //res.render('index', {users: users.list});
+    users.list(req, res, next);
 });
 
 router.get('/user', function(req, res, next) {
-  res.render('user', { title: 'Demo or die' }, {users: users.list});
-  users.list(req, res, next);
-});
-
-router.get('/group', function(req, res, next) {
-  res.render('group', { title: 'Demo or die' }, {users: users.list});
-  users.list(req, res, next);
+    res.render('user', { title: 'Demo or die' });
 });
 
 /*router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});*/
+ res.send('respond with a resource');
+ });*/
 
 module.exports = router;
 
 
 
 /*
-
-module.exports = function getRouter(app) {
-    const router = new express.Router();
-
-    router.route('/')
-        .get(users.list)
-        .post(users.create);
-
-    router.route('/:userId')
-        .get(users.read)
-        .put(users.update)
-        .delete(users.remove);
-
-    router.param('userId', users.findById);
-
-    // Register our routes
-    app.use('/api/v1/users', router);
-};*/
+ module.exports = function getRouter(app) {
+ const router = new express.Router();
+ router.route('/')
+ .get(users.list)
+ .post(users.create);
+ router.route('/:userId')
+ .get(users.read)
+ .put(users.update)
+ .delete(users.remove);
+ router.param('userId', users.findById);
+ // Register our routes
+ app.use('/api/v1/users', router);
+ };*/
